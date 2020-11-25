8c1f0cfc26b7999770c72f1cbe2d05cf8cce607d
Jaime Calvo <jaimecgsm97@gmail.com>
2020-11-25 18:03:44 +0100

    Segundo commit

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-const boton1 = document.createElement('button', "Contar palabras");
+const boton1 = document.createElement('button');
 
 document.getElementById('buttons-container').appendChild(boton1);
 
-const boton2 = document.createElement('button', "Restablecer");
+const boton2 = document.createElement('button');
 
 document.getElementById('buttons-container').appendChild(boton2);
 
-const newDiv = document.createElement('div', "div1");
+const newDiv = document.createElement('div');
+
+document.getElementsByClassName('text-container')[0].appendChild(newDiv);
 
 const nowSpan = document.createElement('span');
 
-document.getElementsByTagName('div1')[0].appendChild(nowSpan);
+document.getElementsByTagName('div')[0].appendChild(nowSpan);
+
+nowSpan.textContent='5';
+
+
+contarpalabras = document.getElementsById("text-container").value;
+
+numeroCaracteres = contar.length;
+
+textoAreaDiv = contarpalabras.split(" ")
+
+boton1.addEventListener('click', () => {
+    window.alert('')
+})
