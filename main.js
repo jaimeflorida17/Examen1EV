4034e6ac2618f09754947201c50c01dba32beac5
Jaime Calvo <jaimecgsm97@gmail.com>
2020-11-25 17:36:52 +0100

    Verificacion 1

---
+++ main.js
@@ -0,0 +1,13 @@
+const boton1 = document.createElement('button', "Contar palabras");
+
+document.getElementById('buttons-container').appendChild(boton1);
+
+const boton2 = document.createElement('button', "Restablecer");
+
+document.getElementById('buttons-container').appendChild(boton2);
+
+const newDiv = document.createElement('div', "div1");
+
+const nowSpan = document.createElement('span');
+
+document.getElementsByTagName('div1')[0].appendChild(nowSpan);
