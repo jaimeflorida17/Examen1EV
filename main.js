df914f1f9eab8586b9bb8eb2ba02e6e29e6f32e3
Jaime Calvo <jaimecgsm97@gmail.com>
2020-11-25 18:53:38 +0100

    ultimo comit

--- main.js
+++ main.js
@@ -10,15 +10,37 @@ const newDiv = document.createElement('div');
 
 document.getElementsByClassName('text-container')[0].appendChild(newDiv);
 
+const contadorDiv = (div) => {
+    let counter = 0;
+    const text = div.textContent.replace('.', '');
+    const texto = text.replace(',', '');
+    const arrayPalabras = texto.split(" ");
+    for (let i = 0; i < arrayPalabras.length; i++) {
+        if (arrayPalabras[i].length > 10) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+
+const contar = () => {
+    newSpan.textContent = contadorDiv(div);
+}
+
+boton1.setAtributte('onclick', 'contar()')
+
 const nowSpan = document.createElement('span');
 
-document.getElementsByTagName('div')[0].appendChild(nowSpan);
+newDiv.appendChild(nowSpan);
 
+/*
 nowSpan.textContent='5';
 
 
 contarpalabras = document.getElementsById("text-container").value;
-
+*/
+/*
 numeroCaracteres = contar.length;
 
 textoAreaDiv = contarpalabras.split(" ")
@@ -26,3 +48,4 @@ textoAreaDiv = contarpalabras.split(" ")
 boton1.addEventListener('click', () => {
     window.alert('')
 })
+*/
